2a83df56181f603f3f64b4aa57e41b8dc0610ce3
Gisele66 <giselelarose@gmail.com>
2026-02-02 22:13:40 +0000

    Clean up repo - keep only MySQL Cheatsheet README and context.md

--- challenge1.2/index.php
+++
@@ -1,9 +0,0 @@
-
-<form method="post" action='day4-api.php'>
-    <input type="text" name='title'><br>
-    <textarea name="content"></textarea><br>
-    <button type="submit">POST</button>
-
-</form>
-
-<hr>
